c5e5c02ec753d6fd793b8271018b90115c9ac8e0
dylan-anz <dylan.butler@anz.com>
2020-11-22 18:43:19 +1100

    tidy

--- Makefile
+++ Makefile
@@ -5,11 +5,10 @@ lint:
 .PHONY: build
 build:
 	go build -o server ./cmd/server
-	go build -o shortener ./cmd/shortener
 
 .PHONY: clean
 clean:
-	rm -f home shortener coverage.out coverage.html
+	rm -f home coverage.out coverage.html
 	go mod tidy
 	go mod vendor
 
@@ -18,15 +17,11 @@ rebuild: clean build
 
 .PHONY: deploy
 deploy:
-	gcloud app deploy app.yaml shortener.yaml dispatch.yaml
+	gcloud app deploy app.yaml
 
 .PHONY: server
 server: rebuild
 	./server
-
-.PHONY: shortener
-shortener: rebuild
-	./shortener
 
 .PHONY: test
 test:
--- internal/site/server.go
+++ internal/site/server.go
@@ -2,73 +2,9 @@ package site
 
 import (
 	"encoding/json"
-	"fmt"
-	"github.com/dbut2/home/internal/shortener"
-	"github.com/dbut2/home/pkg/url"
 	"github.com/go-chi/chi"
 	"net/http"
-	"strings"
 )
-
-type Shortener struct {
-	*shortener.Shortener
-}
-
-func (s *Shortener) Run(port string) {
-	r := chi.NewRouter()
-
-	r.Get("/*", s.lengthen)
-	r.Post("/*", s.shorten)
-
-	err := http.ListenAndServe(":"+port, r)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (s *Shortener) lengthen(w http.ResponseWriter, r *http.Request) {
-	code := strings.TrimPrefix(r.URL.Path, "/")
-	link, err := s.Lengthen(code)
-	if err != nil {
-		http.Redirect(w, r, "404", http.StatusNotFound)
-		return
-	}
-	http.Redirect(w, r, link.String(), http.StatusMovedPermanently)
-}
-
-func (s *Shortener) shorten(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		respondError(w, err)
-		return
-	}
-	link := r.FormValue("url")
-	code := r.FormValue("code")
-	if link == "" {
-		respondError(w, fmt.Errorf("url empty"))
-		return
-	}
-	if code == "" {
-		code = strings.TrimPrefix(r.URL.Path, "/")
-	}
-	u, err := url.New(r.FormValue("url"))
-	if err != nil {
-		respondError(w, err)
-		return
-	}
-	code, err = s.Shorten(u, code)
-	if err != nil {
-		respondError(w, err)
-		return
-	}
-	respondJSON(w, http.StatusOK, struct {
-		URL  string `json:"url"`
-		Code string `json:"code"`
-	}{
-		URL:  link,
-		Code: code,
-	})
-}
 
 type Server struct{}
 
